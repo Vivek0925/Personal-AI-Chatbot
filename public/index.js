da1718ed675a2483f882b10e87e1771b282ba485
akashpatel121 <akashpatel522004@gmail.com>
2025-03-01 00:54:00 +0530

    Google Authentication Code

--- public/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.login').addEventListener('click', (event) => {
-  event.preventDefault(); 
-  window.location.href = 'http://localhost:3000/auth/google';
-});
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const passport = require("passport");
-const session = require("express-session");
-const path = require("path");
-const GoogleStrategy = require
-("passport-google-oauth20").Strategy;
-require("dotenv").config();
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const path = require('path');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"/views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.static(path.join(__dirname, "/public")))
-app.use(session({secret:"secret", resave: false, saveUninitialized:true}));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new GoogleStrategy(
-{
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
-}, 
-(accesToken, refreshToken, profile, done)=>{
-  return done(null, profile);
-}
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accesToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -34,9 +35,9 @@ passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-app.get("/",(req,res)=>{
-  res.sendFile("index.html");
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 app.get(
   '/auth/google',
@@ -47,12 +48,10 @@ app.get(
   '/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    // res.send(`Welcome, ${req.user.displayName}`);
-    res.render("home", {name:req.user.displayName});
+    res.render('home', { name: req.user.displayName });
   }
 );
 
-
-app.listen(3000,()=>{
-  console.log("Server running on http://localhost:3000");
-})
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
